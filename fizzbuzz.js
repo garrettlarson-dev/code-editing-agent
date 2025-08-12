75ea47a256a3c61d1a0b306a3dbbaa10f1ba12ae
Garrett Larson <63696236+garrettlarson-dev@users.noreply.github.com>
2025-08-11 21:41:55 -0600

    init

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,29 @@
 /**
- * FizzBuzz implementation in JavaScript
- * Run with: node fizzbuzz.js
+ * FizzBuzz implementation
+ * Prints numbers from 1 to n, but:
+ * - For multiples of 3, prints "Fizz" instead
+ * - For multiples of 5, prints "Buzz" instead
+ * - For multiples of both 3 and 5, prints "FizzBuzz"
  */
 
-// Get command line arguments (optional)
-const args = process.argv.slice(2);
-const max = args.length > 0 ? parseInt(args[0]) : 100; // Default to 100 if no argument provided
-
-// FizzBuzz implementation
-for (let i = 1; i <= max; i++) {
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
+      console.log("FizzBuzz");
     } else if (i % 3 === 0) {
-        console.log('Fizz');
+      console.log("Fizz");
     } else if (i % 5 === 0) {
-        console.log('Buzz');
+      console.log("Buzz");
     } else {
-        console.log(i);
+      console.log(i);
     }
+  }
 }
+
+// Run FizzBuzz for numbers 1 to 100
+fizzBuzz(100);
+
+// If you want to run with a custom number, you can call the function like:
+// fizzBuzz(15);
+
+console.log("FizzBuzz completed!");
